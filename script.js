11e24333923a2602b3a46e6a80dc49801d038fad
Damian Padilla <damipad4@gmail.com>
2022-09-24 18:08:06 -0500

    Completed dark mode

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const headingColor = document.querySelector('h1');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark Mode
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rbg(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+}
+
+// Light Mode
+function lightMode(){
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+}
 
 // Switch Theme Dynamically
 function switchTheme (event) {
@@ -7,8 +28,10 @@ function switchTheme (event) {
     console.log(isChecked);
     if (isChecked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
