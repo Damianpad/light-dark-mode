1e5f41964169eac66d3da15387d8a51cd089ca8a
Damian Padilla <damipad4@gmail.com>
2022-09-25 09:21:49 -0500

    implemented local storage for theme

--- script.js
+++ script.js
@@ -39,12 +39,25 @@ function switchTheme (event) {
     console.log(isChecked);
     if (isChecked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
-toggleSwitch.addEventListener('change', switchTheme)
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
+console.log(currentTheme);
